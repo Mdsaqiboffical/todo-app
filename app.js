d420aaaa6346e52bde1fe0cd158a173dc6abaa7d
Mdsaqiboffical <154467672+Mdsaqiboffical@users.noreply.github.com>
2024-08-29 14:57:28 +0500

    done

--- app.js
+++ app.js
@@ -21,32 +21,34 @@ addButn.addEventListener("click", function () {
 
             editButn.style.display = "none";
 
-                let changeInput = document.createElement("input");
-                li.appendChild(changeInput);
-                
-                let changeInputButn = document.createElement("Button");
-                changeInputButn.innerHTML = "<i class='fa-solid fa-arrows-rotate'></i>";
-                li.appendChild(changeInputButn);
-    
-                changeInputButn.addEventListener("click", function(){
-                    li.innerHTML = changeInput.value;
+            let changeInput = document.createElement("input");
+            li.appendChild(changeInput);
 
-                    editButn.style.display = "inline"
-                });
+            let changeInputButn = document.createElement("Button");
+            changeInputButn.innerHTML = "<i class='fa-solid fa-arrows-rotate'></i>";
+            li.appendChild(changeInputButn);
+
+            changeInputButn.addEventListener("click", function () {
+                li.innerHTML = changeInput.value;
+
+                editButn.style.display = "inline"
+            });
 
             // li.innerHTML = changeInput.value    
             // let changeValue = prompt("Change the value"); 
             // li.innerHTML = changeValue;
-        }) ;
-    
+        });
+
         // DEL BUTN
         let onceDelButn = document.createElement("button");
         onceDelButn.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
         ul.appendChild(onceDelButn);
         onceDelButn.addEventListener("click", function () {
             ul.removeChild(li);
-            onceDelButn.classList.toggle("hide")
-            editButn.classList.toggle("hide")
+            editButn.style.display = "none";
+            onceDelButn.style.display = "none";
+            // onceDelButn.classList.toggle("hide")
+            // editButn.classList.toggle("hide")
         });
     };
 });
